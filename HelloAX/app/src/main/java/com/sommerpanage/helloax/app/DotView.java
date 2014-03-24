6355a361001b4c8a2aad4fb89754020d825a0fc1
Sommer Panage <spanage@twitter.com>
2014-03-24 00:09:37 -0700

    App complete without AX

--- HelloAX/app/src/main/java/com/sommerpanage/helloax/app/DotView.java
+++ HelloAX/app/src/main/java/com/sommerpanage/helloax/app/DotView.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 
 /**
@@ -14,13 +20,19 @@ import android.view.View;
  */
 public class DotView extends View {
 
-    private final int DEFAULT_DOT_COLOR = R.color.defaultDotColor;
+    private static final int DEFAULT_DOT_COLOR = R.color.defaultDotColor;
 
     private float mDotRadius = R.dimen.default_dot_radius;
 
-    private Paint mDot1Paint;
-    private Paint mDot2Paint;
-    private Paint mDot3Paint;
+    private List<DotDrawing> mDotsArray;
+    private static final List<Paint> sPaintArray = new ArrayList<Paint>();
+
+    private final Random mRandom = new Random();
+
+    private float mOriginX;
+    private float mOriginY;
+    private float mWidth;
+    private float mHeight;
 
     public DotView(Context context) {
         super(context);
@@ -37,35 +49,178 @@ public class DotView extends View {
         init(attrs, defStyle);
     }
 
+    public ArrayList<? extends Parcelable> getDotsState() {
+        return new ArrayList<DotDrawing>(mDotsArray);
+    }
+
+    public void setDotsState(List<? extends Parcelable> dots) {
+        if (!dots.isEmpty() && dots.get(0) instanceof DotDrawing) {
+            mDotsArray = (List<DotDrawing>) dots;
+            invalidate();
+        }
+
+    }
+
+    public void addRandomDot() {
+        mDotsArray.add(new DotDrawing(getRandomDotCenterPoint(true),
+                       getRandomDotCenterPoint(false),
+                       mDotRadius,
+                       getRandomPaintIndex()));
+        invalidate();
+    }
+
+    public void removeLastDot() {
+        if (!mDotsArray.isEmpty()) {
+            mDotsArray.remove(mDotsArray.size() - 1);
+            invalidate();
+        }
+
+    }
+
+    public void clearDots() {
+        mDotsArray.clear();
+        invalidate();
+    }
+
+    private float getRandomDotCenterPoint(boolean isX) {
+        return randomIntInRange((int) getMinDotCenterPoint(true), (int) getMaxDotCenterPoint(false));
+    }
+
+    private float getMinDotCenterPoint(boolean isX) {
+        return (isX) ? (mOriginX + mDotRadius) : (mOriginY + mDotRadius);
+    }
+
+    private float getMaxDotCenterPoint(boolean isX) {
+        return (isX) ? (mOriginX + mWidth - mDotRadius) : (mOriginY + mHeight - mDotRadius);
+    }
+
+    private int getRandomPaintIndex() {
+        return randomIntInRange(0, sPaintArray.size() - 1);
+    }
+
+    private int randomIntInRange(int min, int max) {
+        return mRandom.nextInt((max - min) + 1) + min;
+    }
+
     private void init(AttributeSet attrs, int defStyle) {
 
         final TypedArray a = getContext().obtainStyledAttributes(
                 attrs, R.styleable.DotView, defStyle, 0);
 
-        mDotRadius = a.getDimension(R.styleable.DotView_dotRadius, mDotRadius);
+        if (a != null) {
 
-        int color1 = a.getColor(R.styleable.DotView_dotColor1, DEFAULT_DOT_COLOR);
-        int color2 = a.getColor(R.styleable.DotView_dotColor2, DEFAULT_DOT_COLOR);
-        int color3 = a.getColor(R.styleable.DotView_dotColor3, DEFAULT_DOT_COLOR);
+            mDotRadius = a.getDimension(R.styleable.DotView_dotRadius, mDotRadius);
 
-        mDot1Paint = getPaintForColor(color1);
-        mDot2Paint = getPaintForColor(color2);
-        mDot3Paint = getPaintForColor(color3);
+            int color1 = a.getColor(R.styleable.DotView_dotColor1, DEFAULT_DOT_COLOR);
+            int color2 = a.getColor(R.styleable.DotView_dotColor2, DEFAULT_DOT_COLOR);
+            int color3 = a.getColor(R.styleable.DotView_dotColor3, DEFAULT_DOT_COLOR);
+
+            if (sPaintArray.isEmpty()) {
+                sPaintArray.add(getPaintForColor(color1));
+                sPaintArray.add(getPaintForColor(color2));
+                sPaintArray.add(getPaintForColor(color3));
+            }
+
+        }
+
+        mDotsArray = new ArrayList<DotDrawing>();
     }
 
     private Paint getPaintForColor(int color) {
         Paint paint = new TextPaint();
         paint.setFlags(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(color);
+        paint.setStyle(Paint.Style.FILL);
 
         return paint;
+    }
+
+    @Override
+    protected void onSizeChanged (int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mOriginX = getPaddingLeft();
+        mOriginY = getPaddingTop();
+        mWidth = w - (getPaddingLeft() + getPaddingRight());
+        mHeight = h - (getPaddingTop() + getPaddingBottom());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // TODO: draw!
+        for (DotDrawing dot : mDotsArray) {
+            canvas.drawCircle(dot.getCenterX(), dot.getCenterY(), dot.getRadius(), sPaintArray.get(dot.getPaintIndex()));
+        }
 
+    }
+
+    private class DotDrawing implements Parcelable{
+        private float mCenterX;
+        private float mCenterY;
+        private float mRadius;
+        private int mPaintIndex;
+
+        public DotDrawing(float centerX, float centerY, float radius, int paintIndex) {
+            mCenterX = centerX;
+            mCenterY = centerY;
+            mRadius = radius;
+            mPaintIndex = paintIndex;
+        }
+
+        private DotDrawing(Parcel in) {
+            mCenterX = in.readFloat();
+            mCenterY = in.readFloat();
+            mRadius = in.readFloat();
+            mPaintIndex = in.readInt();
+        }
+
+        public float getCenterX() {
+            return mCenterX;
+        }
+
+        public float getCenterY() {
+            return mCenterY;
+        }
+
+        public void setCenterX(float centerX) {
+            mCenterX = centerX;
+        }
+
+        public void setCenterY(float centerY) {
+            mCenterY = centerY;
+        }
+
+        public float getRadius() {
+            return mRadius;
+        }
+
+        public int getPaintIndex() {
+            return mPaintIndex;
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            dest.writeFloat(mCenterX);
+            dest.writeFloat(mCenterY);
+            dest.writeFloat(mRadius);
+            dest.writeInt(mPaintIndex);
+        }
+
+        public final Parcelable.Creator<DotDrawing> CREATOR
+                = new Parcelable.Creator<DotDrawing>() {
+            public DotDrawing createFromParcel(Parcel in) {
+                return new DotDrawing(in);
+            }
+
+            public DotDrawing[] newArray(int size) {
+                return new DotDrawing[size];
+            }
+        };
     }
 }
--- HelloAX/app/src/main/java/com/sommerpanage/helloax/app/MainActivity.java
+++ HelloAX/app/src/main/java/com/sommerpanage/helloax/app/MainActivity.java
@@ -2,17 +2,22 @@ package com.sommerpanage.helloax.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
+    private static final String DOTS_KEY = "dotsKey";
+
     private Button mMinusButton;
-    private Button mPlusButton;
+    private ImageButton mPlusButton;
+    private Button mClearButton;
+    private DotView mDotView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,44 +25,42 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         mMinusButton = (Button) findViewById(R.id.minus_button);
-        mPlusButton = (Button) findViewById(R.id.plus_button);
+        mPlusButton = (ImageButton) findViewById(R.id.plus_button);
+        mClearButton = (Button) findViewById(R.id.clear_button);
+        mDotView = (DotView) findViewById(R.id.dot_view);
 
         setupButton(mMinusButton);
         setupButton(mPlusButton);
+        setupButton(mClearButton);
     }
 
-    private void setupButton(Button button) {
+    private void setupButton(View button) {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (v == mMinusButton) {
-                    Log.d("MainActivity", "Subtract");
+                    mDotView.removeLastDot();
                 } else if (v == mPlusButton) {
-                    Log.d("MainActivity", "Add");
+                    mDotView.addRandomDot();
+                } else if (v == mClearButton) {
+                    mDotView.clearDots();
                 }
             }
         });
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onSaveInstanceState (Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putParcelableArrayList(DOTS_KEY, mDotView.getDotsState());
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onRestoreInstanceState (Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            ArrayList<? extends Parcelable> dots = savedInstanceState.getParcelableArrayList(DOTS_KEY);
+            mDotView.setDotsState(dots);
         }
-        return super.onOptionsItemSelected(item);
+        super.onRestoreInstanceState(savedInstanceState);
     }
-
 }
